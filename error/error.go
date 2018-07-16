8eeb685b6249a553a9ef9ffa241b773e789ffa37
Mike Gardner <mjgardner@abitofhelp.com>
2018-07-16 16:54:30 -0700

    Corrected the file headers to indicate an MIT license rather than an BSD one.

--- error/error.go
+++ error/error.go
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2018 A Bit of Help, Inc. - All Rights Reserved, Worldwide.
-// Use of this source code is governed by a BSD-style  license that can be found in the LICENSE file.
+// Use of this source code is governed by a MIT  license that can be found in the LICENSE file.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Package error implements helpful utility functions for error processing.
@@ -11,14 +11,26 @@ import (
 	"os"
 )
 
-// Func Must determines whether an error has happened.
-// It returns the err value.
-func Must(err error) error {
+// Type OnErrorFunc defines an optional function to call after handling the error.
+type OnErrorFunc func(err error)
+
+// Func OnError determines whether an error has happened.
+// If there is an error, it is logged, and an optional function can be invoked.
+// On error, false is returned to the caller, otherwise, true.
+func IsError(err error, onError OnErrorFunc) bool {
 	if err != nil {
+		// Log the error...
 		LogError("", err)
+
+		// Do additional handling of the error, as implemented by the caller.
+		if onError != nil {
+			onError(err)
+		}
+
+		return true
 	}
 
-	return err
+	return false
 }
 
 // Implement the ILogger interface.
--- time/time.go
+++ time/time.go
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2018 A Bit of Help, Inc. - All Rights Reserved, Worldwide.
-// Use of this source code is governed by a BSD-style  license that can be found in the LICENSE file.
+// Use of this source code is governed by a MIT  license that can be found in the LICENSE file.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Package time contains helpful methods that extend the runtime's time package.
